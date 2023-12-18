cf30fa40912727898a91510686646c2c8b391488
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-17 23:48:45 -0500

    Added all View functionalities

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 
+let sql;
 const questions = [{
           name: "userChoice",
           message: "Welcome to VS Workforce Tracker!\nPlease choose the options from the below: ",
@@ -29,8 +30,28 @@ inquirer
                     console.log(`Connected to the workforcetracker_db database.`)
                );
 
-               db.query('SELECT dept_id as "Department ID", dept_name as "Department Name" FROM wt_department', function (err, results) {
+               if (answers.userChoice === "View all Department") {
+                    sql = 'SELECT dept_id as "Department ID", dept_name as "Department Name" FROM wt_department';                 
+               } else if (answers.userChoice === "View all Roles") {
+                    sql = 'SELECT a.role_id as "Role ID", a.job_title as "Job Title", b.dept_name as "Department Name", a.salary as "Salary" '+
+                                   'FROM wt_role a JOIN wt_department b '+
+                                   'ON a.dept_id = b.dept_id';
+               } else if (answers.userChoice === "View all Employees") {
+                    sql = 'SELECT a.emp_id as "Employee ID", a.first_name as "First Name", a.last_name as "Last Name", '+ 
+                              'b.job_title as "Title", c.dept_name as "Department", b.salary as "Salary", CONCAT(e.first_name, " ", e.last_name) as "Manager" '+
+                              'FROM wt_employee a JOIN wt_role b '+
+                              'ON a.emp_role = b.role_id '+
+                              'JOIN wt_department c '+
+                              'ON b.dept_id = c.dept_id '+
+                              'JOIN wt_hierarchy_relation d '+
+                              'ON a.emp_id = d.emp_id '+
+                              'left outer JOIN wt_employee e '+
+                              'ON d.manager_id = e.emp_id '+
+                              'ORDER BY a.emp_id ';
+               }
+
+               db.query(sql, function (err, results) {
                     return console.table(results);
-               });
+               });      
           }
      });
